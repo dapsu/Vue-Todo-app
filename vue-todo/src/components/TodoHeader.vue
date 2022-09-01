75c08488a836283ab512a295cd3cef46f9fa2224
dapsu <greenrock46@gmail.com>
2022-09-01 23:23:55 +0900

    Feat: 컴포넌트 생성 및 등록 * 파비콘, 아이콘, 폰트, 반응형 태그 설정 * App.vue에 컴포넌트 등록

---
+++ vue-todo/src/components/TodoHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <header>
+      <h1>TODO it!</h1>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped> /** scoped: 해당 컴포넌트 안에서만 유요한 스타일로 지정 */
+  h1 {
+    color: #2F3B52;
+    font-weight: 900;
+    margin: 2.5rem 0 1.5rem;
+  }
+</style>
