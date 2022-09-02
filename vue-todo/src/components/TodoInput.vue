b8ca3982c478bed8dd8d717d75a7124e5b9eb3c4
dapsu <greenrock46@gmail.com>
2022-09-02 10:50:45 +0900

    Feat: TodoInput.vue 작성

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -1,15 +1,56 @@
 <template>
-  <div>
-    input
+  <div class="inputBox shadow">
+    <input type="text" v-model="newTodoItem" v-on:keyup.enter="adTodo">
+    <span class="addContainer" v-on:click="addTodo">
+      <i class="fa-solid fa-plus addBtn"></i>
+    </span>
   </div>
 </template>
 
 <script>
 export default {
-
+  data: function() {
+    return {
+      newTodoItem:''
+    }
+  },
+  methods: {
+    addTodo: function() {
+      console.log(this.newTodoItem);
+      // 저장하는 로직
+      localStorage.setItem('intputValue', this.newTodoItem);  // 로컬스토리지에 저장
+      this.clearInput();
+    },
+    clearInput: function() {
+      this.newTodoItem = '';  // 저장 이후 인풋창에 작성된 값 지우기
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+  input:focus {
+    outline: none;
+  }
+  .inputBox {
+    background: white;
+    height: 50px;
+    line-height: 50px;
+    border-radius: 5px;
+  }
+  .inputBox input {
+    border-style: none;
+    font-size: 2rem;
+  }
+  .addContainer {
+    float: right;
+    background: linear-gradient(to right, #6478FB, #8763FB);
+    display: block;
+    width: 3rem;
+    border-radius: 0 5px 5px 0;
+  }
+  .addBtn {
+    color: white;
+    vertical-align: middle;
+  }
 </style>
